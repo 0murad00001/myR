08c563c475f364f1748ada24bf47298045dbdeda
Murad <0murad00001@gmail.com>
2022-07-10 09:37:20 +0300

    Lesson 1-4

--- src/www.java
+++ src/www.java
@@ -4,6 +4,6 @@ public class www {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("dfef");
-        System.out.println("dfef");
+        System.out.println("dfef");System.out.println("dfef");
     }
 }
